13e343b93ce379839cc3f9c2d9db5723405556e2
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-11-28 11:31:18 +0100

    Add files via upload

---
+++ com/cc/java/assoziation/App.java
@@ -0,0 +1,17 @@
+package com.cc.java.assoziation;
+
+public class App {
+    
+    public static void main(String[] args) {
+     
+        Customer customer = new Customer(1, "Maxine Mützerich", "Mützenhausen");
+       
+        customer.initOrder();
+        customer.checkOrder();
+        
+
+       
+    }
+
+}
+
